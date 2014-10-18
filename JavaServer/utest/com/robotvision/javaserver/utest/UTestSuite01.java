8ea997e4bf8869d0239c88a94748349d06cfe0ef
Shirui Ma <mashirui521@gmail.com>
2014-10-18 04:16:23 +0200

    modify test for standard work flow

--- JavaServer/utest/com/robotvision/javaserver/utest/UTestSuite01.java
+++ JavaServer/utest/com/robotvision/javaserver/utest/UTestSuite01.java
@@ -1,5 +1,6 @@
 package com.robotvision.javaserver.utest;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
@@ -12,11 +13,16 @@ import com.robotvision.javaserver.utils.Commands;
 
 public class UTestSuite01 {
 
+	/**
+	 * Test standard work flow
+	 */
 	@Test
 	public void UTest01() {
 		IServerSupporter supporter = ServerSupporter.getInstance(8888);
+		// find client
 		supporter.findClient();
 		
+		// get client ip and port number
 		try {
 			String address = supporter.receiveClientAddress();
 			System.out.println(address);
@@ -26,26 +32,44 @@ public class UTestSuite01 {
 			fail("fail to receive ip: " + e.getLocalizedMessage());
 		}
 		
+		// send login
 		try {
 			supporter.send(Commands.LOGIN);
 		} catch (Exception e) {
 			fail("fail to send login command: " + e.getLocalizedMessage());
 		}
 		
+		// receiving picture
 		try {
+			// check camera availability
 			if (supporter.receiveCameraAvailable()) {
+				
+				// send capture command
+				System.out.println("client is available.");
+				System.out.print("sending capture picture command...");
 				supporter.send(Commands.CAPTURE_PICTURE);
+				System.out.println("...OK");
+				
+				// waiting for picture
+				System.out.print("getting picture...");
 				supporter.receivePicture();
-				System.out.println("captured");
+				System.out.println("...captured");
 				supporter.send(Commands.STOP_CAPTURE);
 			}
 		} catch (Exception e) {
 			fail("fail to capture: " + e.getMessage());
+			System.out.println("...FAIL");
 		}
 		
+		// adapt picture to RGB
 		supporter.adaptByteToRGB();
-		supporter.getR();
-		supporter.getG();
-		supporter.getB();
+		int[] R = supporter.getR();
+		int[] G = supporter.getG();
+		int[] B = supporter.getB();
+		
+		// check the received picture data, the picture size should be 640*480
+		assertEquals("invalid R.", 307200, R.length);
+		assertEquals("invalid G.", 307200, G.length);
+		assertEquals("invalid B.", 307200, B.length);
 	}
 }
